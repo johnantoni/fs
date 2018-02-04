1d2855ed99681362bfbd8cf6178daf78d32995eb
John Griffiths <john@uxgent.com>
2018-02-04 16:21:49 -0500

    code.

--- react-app-unejected/src/App.js
+++ react-app-unejected/src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-class App extends Component {
+class Greeting extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <h1>Welcome to my App</h1>
+    )
   }
 }
 
-export default App;
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      potato: 'i am a potato',
+      version: '0.11.3',
+      build: '6'
+    }
+  }
+
+  handleClick = () => {
+    alert(`oh god!!! what have you done!!! we're all gonna die`)
+  }
+
+  render() {
+    return (
+      <div>
+        <Greeting/>
+        <p>
+          Do not press this <button onClick={this.handleClick}>button</button>
+        </p>
+        <h4>{this.state.potato}</h4>
+        {/* i am a comment, if you see me somethings wrong!! */}
+        <footer>
+          <p>V.{this.state.version}, build {this.state.build}</p>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default App
