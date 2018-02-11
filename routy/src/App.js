3ad786e0ae5b0c83a25a6d8adfdfb453765d01d5
John Griffiths <john@uxgent.com>
2018-02-11 16:38:00 -0500

    routey.

--- routy/src/App.js
+++ routy/src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  )
+}
 
 const About = () => {
   return (
     <div>
-      About
+      <h2>About</h2>
+    </div>
+  )
+}
+
+const Socks = (props) => {
+  return (
+    <h3>{props.match.params.product}</h3>
+  )
+}
+
+const Store = ({ match }) => {
+  console.log(match)
+  return (
+    <div>
+      <h2>Store</h2>
+      <Link to={`/store/socks`}>Socks</Link>&nbsp;
+      <Link to={`/store/cardy`}>Cardy</Link>&nbsp;
+      <Link to={`/store/50cal`}>50 Cal</Link>
     </div>
   )
 }
@@ -15,7 +41,13 @@ class App extends Component {
       <Router>
         <div>
           <h1>Routing</h1>
-          <Route path='/about' component={About} />
+          <Link to="/">Home</Link>&nbsp;
+          <Link to="/about">About</Link>&nbsp;
+          <Link to="/store">Store</Link>&nbsp;
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route path='/store' component={Store} />
+          <Route path='/store/:product' component={Socks} />
         </div>
       </Router>
     );
