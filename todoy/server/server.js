19ecb311dfacb9abc4380273c58f5af7a59c171d
John Griffiths <john@uxgent.com>
2018-02-11 17:41:49 -0500

    remove.

--- todoy/server/server.js
+++ todoy/server/server.js
@@ -7,29 +7,56 @@ const todos = [
   { "3": "get more coffee" }
 ]
 
-// curl 'http://localhost:8080/'
+// curl -X GET 'http://localhost:8080/'
 
 app.get("/", (req, res) => {
   res.status(200).send("boo")
 })
 
-// curl 'http://localhost:8080/todos'
+// curl -X GET 'http://localhost:8080/todos'
 
 app.get("/todos", (req, res) => {
-  res.status(200).send(todos)
+  res.status(200).json({
+    success: true,
+    todos: todos
+  })
 })
 
-// curl 'http://localhost:8080/todo/2'
+// curl -X GET 'http://localhost:8080/todo/2'
 
 app.get("/todo/:id", (req, res) => {
-  res.status(200).send(todos[req.params.id])
+  res.status(200).json({
+    success: true,
+    todos: todos[req.params.id]
+  })
 })
 
 // curl -X POST 'http://localhost:8080/todos/5'
 
 app.post("/todos/:id", (req, res) => {
   todos.push(req.params.id)
-  res.status(200).send(todos)
+  res.status(200).json({
+    success: true,
+    todos: todos
+  })
+})
+
+// curl -X DELETE 'http://localhost:8080/todos/5'
+
+app.delete("/todos/:id", (req, res) => {
+  let removed = todos.splice(req.params.id)
+
+  if (removed.length === 0) {
+    res.status(404).json({
+      success: false,
+      message: "error!!!!"
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    todos: removed
+  })
 })
 
 app.listen(8080)
