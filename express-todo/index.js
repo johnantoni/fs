78b36e521545d94bfee049610ca045d106ac03c1
John Griffiths <john@uxgent.com>
2018-02-25 13:57:36 -0500

    todos.

--- express-todo/index.js
+++ express-todo/index.js
@@ -18,30 +18,29 @@ app.post('/todos/:todo', (req, res) => {
   const todo = req.params.todo
   todos.push(todo)
   res
-    .status(200)
-    .json({ todos : todos })
+    .status(201)
+    .json({ todo : todo })
 })
 
 // put
 // curl -X PUT "http://localhost:8080/todos/0/cheese"
 app.put('/todos/:index/:todo', (req, res) => {
-  const index = req.params.index
-  const todo = req.params.todo
+  const { index, todo } = req.params
   todos[index] = todo
   res
-    .status(200)
-    .json({ todos : todos })
+    .status(201)
+    .json({ todo : todo })
 })
-
 
 // delete
 // curl -X DELETE "http://localhost:8080/todos/0"
 app.delete('/todos/:index', (req, res) => {
   const index = req.params.index
+  const deletedTodo = todos[index]
   todos.splice(index, 1);
   res
-    .status(200)
-    .json({ todos : todos })
+    .status(202)
+    .json({ todo : deletedTodo })
 })
 
 app.listen(PORT)
