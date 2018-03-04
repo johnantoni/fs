a06bfbea1f24ca149be38ea51521a83ac0ba9901
John Griffiths <john@uxgent.com>
2018-03-04 13:05:03 -0500

    webpack 4.

---
+++ webpack/src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import { render } from "react-dom"
+
+const App = () => {
+  return (<div>
+    <h1>hello world</h1>
+    <h2>supercalifragilisticexpialidocious</h2>
+  </div>)
+}
+
+render(<App/>, document.getElementById('app'));
